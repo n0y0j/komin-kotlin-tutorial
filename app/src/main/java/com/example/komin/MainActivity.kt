b7d6648d50371fa2fae3afaaa4abc8cc25a3e401
n0y0j <n0y0j77@gmail.com>
2020-09-13 16:05:18 +0900

    feat: Create gridview using GridviewAdapter

--- app/src/main/java/com/example/komin/MainActivity.kt
+++ app/src/main/java/com/example/komin/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img = arrayOf(
+            R.drawable.ai,
+            R.drawable.css,
+            R.drawable.html,
+            R.drawable.id,
+            R.drawable.jpg,
+            R.drawable.js,
+            R.drawable.mp4,
+            R.drawable.tiff,
+            R.drawable.php,
+            R.drawable.pdf,
+            R.drawable.jpg,
+            R.drawable.psd
+        )
+
+        val text = arrayOf(
+            "ai",
+            "css",
+            "html",
+            "id",
+            "jpg",
+            "js",
+            "mp4",
+            "tiff",
+            "php",
+            "pdf",
+            "jpg",
+            "psd"
+        )
+
+        val gridviewAdapter = GridviewAdapter(this, img, text)
+        gridview.adapter = gridviewAdapter
 
         viewpager = findViewById(R.id.viewpager) as ViewPager
 
