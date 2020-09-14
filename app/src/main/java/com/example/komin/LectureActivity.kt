a3e9d6273dc91884be2037b284b39a9e1dbd4ad9
n0y0j <n0y0j77@gmail.com>
2020-09-14 13:26:27 +0900

    feat: Create tab layout

--- app/src/main/java/com/example/komin/LectureActivity.kt
+++ app/src/main/java/com/example/komin/LectureActivity.kt
@@ -2,8 +2,12 @@ package com.example.komin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import com.example.komin.fragment.FragmentAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_lecture.*
+import kotlinx.android.synthetic.main.custom_tab.view.*
 
 class LectureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,39 @@ class LectureActivity : AppCompatActivity() {
 
         val fragmentAdapter = FragmentAdapter(supportFragmentManager)
         list_viewpager.adapter = fragmentAdapter
+
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("AI")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("CSS")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("HTML")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("ID")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("JPG")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("JS")))
+
+        list_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+
+        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                if (p0 != null) {
+                    list_viewpager.currentItem = p0.position
+                }
+            }
+
+        })
+
+    }
+
+    private fun createTabView(tabName: String) : View {
+        val tabView = LayoutInflater.from(baseContext).inflate(R.layout.custom_tab, null)
+        tabView.txt_name.text = tabName
+
+        return tabView
     }
 }
